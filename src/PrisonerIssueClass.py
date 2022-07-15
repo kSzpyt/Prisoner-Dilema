b0c50d68db4fdeabbafddd55851251d10626daa9
Karol Szpyt <karolo.szpyt@gmail.com>
2022-07-15 23:47:01 +0200

    added optional print to perform_loop_search

--- src/PrisonerIssueClass.py
+++ src/PrisonerIssueClass.py
@@ -7,11 +7,12 @@ class PrisonerIssue:
         self.prisoner_number_in_box = random.sample(range(1,101),100)
         self.prisoner_number = 1
     
-    def perform_loop_search(self, starting_prisoner: int = None) -> int:
+    def perform_loop_search(self, starting_prisoner: int = None, print_process:bool = True) -> int:
         """method that allows to check how many iterations 
 
         Args:
             starting_prisoner (int, optional): Can specify which prisoner to use. Defaults to 1.
+            print_process (bool, True): If True it will print the go through loop process. Defaults to True.
 
         Raises:
             Exception: When something gone wrong and there is an endless loop
@@ -29,7 +30,10 @@ class PrisonerIssue:
         
         while found_flag==False:
             current_number = self.prisoner_number_in_box[box_number_to_check]
-            print(f"{i}: Box {box_number_to_check + 1} - Number inside {current_number}")
+            
+            if print_process:
+                print(f"{i:02d}:: Box {(box_number_to_check + 1):02d} - Prisonser {current_number}")
+            
             if current_number == self.prisoner_number:
                 found_flag = True
             else:
@@ -42,5 +46,5 @@ class PrisonerIssue:
         
 if __name__ == '__main__':
     issue = PrisonerIssue()
-    issue.perform_loop_search()
+    issue.perform_loop_search(print_process="a")
     
