3d3afd29e64d17c4b0b4ba9a08e23a0d79f06d9e
Karol Szpyt <karolo.szpyt@gmail.com>
2022-07-16 01:03:46 +0200

    simulation

--- src/PrisonerIssueClass.py
+++ src/PrisonerIssueClass.py
@@ -4,8 +4,8 @@ import time
 
 class PrisonerIssue:
     def __init__(self) -> None:
-        self.prisoner_number_in_box = random.sample(range(1,101),100)
-        self.prisoner_number = 1
+        # self.prisoner_number_in_box = random.sample(range(1,101),100)
+        self.prisoners = [x for x in range(1,101)]
     
     def perform_loop_search(self, starting_prisoner: int = None, print_process:bool = True) -> int:
         """method that allows to check how many iterations 
@@ -26,25 +26,54 @@ class PrisonerIssue:
             box_number_to_check = 0
             
         found_flag: bool = False
-        i = 1
+        i = 0
         
         while found_flag==False:
+            i += 1
             current_number = self.prisoner_number_in_box[box_number_to_check]
             
             if print_process:
-                print(f"{i:02d}:: Box {(box_number_to_check + 1):02d} - Prisonser {current_number}")
+                print(f"{i:03d}:: Box {(box_number_to_check + 1):03d} - Prisonser {current_number:02d}")
             
-            if current_number == self.prisoner_number:
+            if current_number == starting_prisoner:
                 found_flag = True
             else:
                 box_number_to_check = current_number - 1
-            i += 1
+            
             # time.sleep(0.2)r
             if i > 100:
                 raise Exception("whoah! Something gone wrong!")
+        if print_process:
+            print("\n")
         return i
+    
+    
+    def single_run(self) -> bool:
+        self.outcome = []
+        for x in self.prisoners:
+            self.outcome.append(PrisonerIssue.perform_loop_search(self, starting_prisoner = x, print_process=False))
         
+        for x in self.outcome:
+            if x > 50:
+                return False
+            else:
+                return True
+    
+    def simulation(self, n):
+        output = []
+        for i in range(n):
+            self.prisoner_number_in_box = random.sample(range(1,101),100)
+            output.append(PrisonerIssue.single_run(self))
+        
+        return output
+        
+        
+        
+            
 if __name__ == '__main__':
     issue = PrisonerIssue()
-    issue.perform_loop_search(print_process="a")
-    
+    n = 1000000
+    otpt = issue.simulation(n)
+    print(sum(otpt)/n)
+    # issue.perform_loop_search(starting_prisoner = 1)
+    # issue.perform_loop_search(starting_prisoner = 2)
