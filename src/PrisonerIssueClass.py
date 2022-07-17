3a246afa5f3ee029db66f24d429d74009aa132df
Karol Szpyt <karolo.szpyt@gmail.com>
2022-07-17 14:54:35 +0200

    small adjustments

--- src/PrisonerIssueClass.py
+++ src/PrisonerIssueClass.py
@@ -2,12 +2,15 @@ import numpy as np
 import random
 import time
 
+
 class PrisonerIssue:
-    def __init__(self) -> None:
-        # self.prisoner_number_in_box = random.sample(range(1,101),100)
-        self.prisoners = [x for x in range(1,101)]
-    
-    def perform_loop_search(self, starting_prisoner: int = None, print_process:bool = True) -> int:
+    def __init__(self, prisoners_count) -> None:
+        self.prisoners_count = prisoners_count
+        self.prisoners = [x for x in range(1, self.prisoners_count + 1)]
+        self.prisoner_number_in_box = random.sample(
+            range(1, self.prisoners_count + 1), self.prisoners_count)
+
+    def perform_loop_search(self, starting_prisoner: int = None, print_process: bool = True, slow_time: float = None) -> int:
         """method that allows to check how many iterations 
 
         Args:
@@ -24,56 +27,57 @@ class PrisonerIssue:
             box_number_to_check = starting_prisoner - 1
         else:
             box_number_to_check = 0
-            
+
         found_flag: bool = False
         i = 0
-        
-        while found_flag==False:
+
+        while found_flag == False:
             i += 1
             current_number = self.prisoner_number_in_box[box_number_to_check]
-            
+
             if print_process:
-                print(f"{i:03d}:: Box {(box_number_to_check + 1):03d} - Prisonser {current_number:02d}")
-            
+                print(
+                    f"{i:03d}:: Box {(box_number_to_check + 1):03d} - Prisonser {current_number:02d}")
+                if i == int(self.prisoners_count/2):
+                    print("DEAD X_X")
             if current_number == starting_prisoner:
                 found_flag = True
             else:
                 box_number_to_check = current_number - 1
-            
-            # time.sleep(0.2)r
-            if i > 100:
+
+            if slow_time:
+                time.sleep(slow_time)
+            if i > self.prisoners_count:
                 raise Exception("whoah! Something gone wrong!")
         if print_process:
             print("\n")
         return i
-    
-    
+
     def single_run(self) -> bool:
         self.outcome = []
         for x in self.prisoners:
-            self.outcome.append(PrisonerIssue.perform_loop_search(self, starting_prisoner = x, print_process=False))
-        
+            self.outcome.append(PrisonerIssue.perform_loop_search(
+                self, starting_prisoner=x, print_process=False))
+
         for x in self.outcome:
-            if x > 50:
+            if x > int(self.prisoners_count/2):
                 return False
-            else:
-                return True
-    
+        return True
+
     def simulation(self, n):
         output = []
         for i in range(n):
-            self.prisoner_number_in_box = random.sample(range(1,101),100)
+            self.prisoner_number_in_box = random.sample(
+                range(1, self.prisoners_count + 1), self.prisoners_count)
             output.append(PrisonerIssue.single_run(self))
-        
+
         return output
-        
-        
-        
-            
+
+
 if __name__ == '__main__':
-    issue = PrisonerIssue()
-    n = 1000000
-    otpt = issue.simulation(n)
-    print(sum(otpt)/n)
-    # issue.perform_loop_search(starting_prisoner = 1)
+    issue = PrisonerIssue(prisoners_count=50)
+    # n = 1000
+    # otpt = issue.simulation(n)
+    # print(sum(otpt)/n)
+    issue.perform_loop_search(starting_prisoner=1, slow_time=0.15)
     # issue.perform_loop_search(starting_prisoner = 2)
